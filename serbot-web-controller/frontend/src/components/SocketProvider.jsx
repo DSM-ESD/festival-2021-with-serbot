95f0c0bc03e9be2eef490f448644a5b4b7c01d8a
Yunseo Hwang <yunseo.dev@gmail.com>
2021-06-30 23:22:30 +0900

    fix: fix cors error

--- serbot-web-controller/frontend/src/components/SocketProvider.jsx
+++ serbot-web-controller/frontend/src/components/SocketProvider.jsx
@@ -5,7 +5,7 @@ import socketio from 'socket.io-client';
 export const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
-  const socketUrl = 'http://localhost:5000';
+  const socketUrl = 'http://127.0.0.1:5000';
   const socket = useRef(null);
 
   if (!socket.current) {
